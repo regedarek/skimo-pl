c34dd109d1e7b0dcfb9d363a3779370538e14aed
Darek Finster <dariusz.finster@gmail.com>
2020-06-12 21:31:35 +0200

    update

--- skimo-pl/app/javascript/packs/application.js
+++ skimo-pl/app/javascript/packs/application.js
@@ -8,8 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'css/pages'
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
